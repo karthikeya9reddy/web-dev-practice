03acb8d1f6346492b2a85c327d6c9ba5d092d20b
karthikeya9reddy <ammulukarthik9@gmail.com>
2026-08-25 14:30:44 +0530

    web dev course

---
+++ Front-end development/js/video 34/script.js
@@ -0,0 +1,22 @@
+alert("hello world im karthik")
+
+console.log("code is running...")
+console.log("code is also running...")
+console.log("code is running like a wow...")
+
+var a = prompt("enter your number")
+var isTrue = confirm("are you sure you want to leave this page and blast your computer ")
+
+if(isTrue){
+   alert("computer is blasting")
+}
+
+else{
+    alert("computer is not blasting")
+}
+
+console.log("your number is " + a)
+
+document.title = "hey i am good"
+
+// document.body.style.backgroundColor = "red"
